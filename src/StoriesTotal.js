38adbdc41a38198cb9a154f7d510b7ece221be50
Ivan Krechetov <ikr@ikr.su>
2015-12-04 14:53:29 +0100

    Lint StoriesTotal

--- src/StoriesTotal.js
+++ src/StoriesTotal.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import estimateTasks  from './estimateTasks';
+import estimateTasks from './estimateTasks';
 
 class StoriesTotal extends React.Component {
     render() {
         const estimation = estimateTasks(this.props.allTasks);
 
         return (
-            <p className="lead">
+            <p className='lead'>
                 Grand total: 75%-likely doable within {estimation} h
             </p>
         );
