6d016f97c0eb7eb0a02de9a406d98b3337fb6d96
Ivan Krechetov <ikr@ikr.su>
2015-12-04 14:50:56 +0100

    Refactor & lint StoryTasks

--- src/StoryTasks.js
+++ src/StoryTasks.js
@@ -1,32 +1,20 @@
 import React from 'react';
 
-const title = (task) => {
-    return task[0];
-};
-
-const bestCaseHours = (task) => {
-    return task[1];
-};
-
-const worstCaseHours = (task) => {
-    return task[2];
-};
+function row([title, bestCaseHours, worstCaseHours], index) {
+    return (
+        <tr key={`k${index}`}>
+            <td>{title}</td>
+            <td>{bestCaseHours}-{worstCaseHours}</td>
+        </tr>
+    );
+}
 
 class StoryTasks extends React.Component {
     render() {
-        const row = (task, index) => {
-            return (
-                <tr key={'k' + index}>
-                    <td>{title(task)}</td>
-                    <td>{bestCaseHours(task)}-{worstCaseHours(task)}</td>
-                </tr>
-            );
-        },
-
-        rows = this.props.tasks.map(row);
+        const rows = this.props.tasks.map(row);
 
         return (
-            <table className="table table-striped">
+            <table className='table table-striped'>
                 <tr>
                     <th>Task title</th>
                     <th>Estimation range (hours)</th>
