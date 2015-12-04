e66193962db85b40bdfe731d6e5e080e3ca5f343
Ivan Krechetov <ikr@ikr.su>
2015-12-04 14:38:46 +0100

    Lint the top level files

--- content.js
+++ content.js
@@ -1,9 +1,10 @@
-export default () => {
-    return [{
-        narrative: require('./stories/one/narrative.md'),
-        tasks:     require('./stories/one/tasks.json')
-    }, {
-        narrative: require('./stories/two/narrative.md'),
-        tasks:     require('./stories/two/tasks.json')
-    }];
-};
+/* eslint global-require: 0 */
+/* global require */
+
+export default () => [{
+    narrative: require('./stories/one/narrative.md'),
+    tasks: require('./stories/one/tasks.json')
+}, {
+    narrative: require('./stories/two/narrative.md'),
+    tasks: require('./stories/two/tasks.json')
+}];
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-import React          from 'react';
-import Story          from './src/Story';
-import StoriesTotal   from './src/StoriesTotal';
+import React from 'react';
+import Story from './src/Story';
+import StoriesTotal from './src/StoriesTotal';
 import allActiveTasks from './src/allActiveTasks';
-import content        from './content'
+import content from './content';
 
-const storiesState = (length) => {
-    return new Array(length).fill({active: true, displayTasks: false});
-};
+function storiesState(count) {
+    return new Array(count).fill({active: true, displayTasks: false});
+}
 
 class Container extends React.Component {
     constructor(props) {
@@ -15,59 +15,69 @@ class Container extends React.Component {
     }
 
     render() {
-        const stories = this.props.stories.map((story, index) => {
-                const onChangeActivity = () => {
-                        this.toggleStoryActivity(index);
-                    },
-
-                    onToggleTasks = () => {
-                      this.toggleStoryTasks(index);
-                    },
-
-                    state = this.state.stories[index];
-
-                return <Story
-                    key={'k' + index}
-                    narrative={story.narrative}
-                    tasks={story.tasks}
-                    active={state.active}
-                    displayTasks={state.displayTasks}
-                    onChangeActivity={onChangeActivity}
-                    onToggleTasks={onToggleTasks}/>;
-            }),
-
-            tasks = allActiveTasks(this.props.stories, this.state.stories);
+        const tasks = allActiveTasks(this.props.stories, this.state.stories);
 
         return (
-            <div className="container">
+            <div className='container'>
                 <h1>Sample user stories</h1>
-                {stories}
+                {this.stories()}
                 <StoriesTotal allTasks={tasks}/>
 
-                <ul className="footer text-right list-unstyled">
+                <ul className='footer text-right list-unstyled'>
                     <li>May 2015</li>
 
                     <li>
-                        Ivan Krechetov &lt;<a href="mailto:ikr@ikr.su">ikr@ikr.su</a>&gt;
+                        Ivan Krechetov &lt;<a href='mailto:ikr@ikr.su'>ikr@ikr.su</a>&gt;
                     </li>
                 </ul>
             </div>
         );
     }
 
+    stories() {
+        return this.props.stories.map((story, index) => {
+            const onChangeActivity = () => {
+                    this.toggleStoryActivity(index);
+                },
+
+                onToggleTasks = () => {
+                    this.toggleStoryTasks(index);
+                },
+
+                state = this.state.stories[index];
+
+            return <Story
+                key={`k${index}`}
+                narrative={story.narrative}
+                tasks={story.tasks}
+                active={state.active}
+                displayTasks={state.displayTasks}
+                onChangeActivity={onChangeActivity}
+                onToggleTasks={onToggleTasks}/>;
+        });
+    }
+
     toggleStoryActivity(index) {
-        let stories = this.state.stories.slice();
+        const stories = this.state.stories.slice();
+
         stories[index] = Object.assign({}, stories[index], {active: !stories[index].active});
         this.setState({stories});
     }
 
     toggleStoryTasks(index) {
-        let stories = this.state.stories.slice();
-        stories[index] = Object.assign({}, stories[index], {displayTasks: !stories[index].displayTasks});
+        const stories = this.state.stories.slice();
+
+        stories[index] = Object.assign(
+            {},
+            stories[index],
+            {displayTasks: !stories[index].displayTasks}
+        );
+
         this.setState({stories});
     }
 }
 
 Container.defaultProps = {stories: content()};
 
+/* global global */
 React.render(React.createElement(Container), global.document.body);
