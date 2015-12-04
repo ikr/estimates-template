95ace6f9df56e3ea9694cfd359c04b71a544fbfe
Ivan Krechetov <ikr@ikr.su>
2015-12-04 14:56:54 +0100

    Lint Story

--- src/Story.js
+++ src/Story.js
@@ -1,34 +1,36 @@
-import React          from 'react';
+import React from 'react';
 import StoryNarrative from './StoryNarrative';
-import StoryTasks     from './StoryTasks';
-import estimateTasks  from './estimateTasks';
+import StoryTasks from './StoryTasks';
+import estimateTasks from './estimateTasks';
 
 class Story extends React.Component {
     render() {
-        const tasksToggleTitle = (this.props.displayTasks ? 'Hide tasks' : 'Show tasks'),
-              elTasks = (this.props.displayTasks? <StoryTasks tasks={this.props.tasks}/> : null),
-              estimation = estimateTasks(this.props.tasks),
-              estimationTextStyle = (this.props.active ? 'bg-success' : 'text-muted');
+        const tasksToggleTitle = (this.props.displayTasks ? 'Hide tasks' : 'Show tasks');
+        const elTasks = (this.props.displayTasks ? <StoryTasks tasks={this.props.tasks}/> : null);
+        const estimation = estimateTasks(this.props.tasks);
+        const estimationTextStyle = (this.props.active ? 'bg-success' : 'text-muted');
+
+        /* eslint no-script-url: 0 */
 
         return (
             <div>
                 <StoryNarrative markdown={this.props.narrative}/>
                 {elTasks}
 
-                <div className="row">
-                    <div className="col-md-2">
+                <div className='row'>
+                    <div className='col-md-2'>
                         <a
-                        className="btn btn-default"
-                        href="javascript:;"
+                        className='btn btn-default'
+                        href='javascript:;'
                         onClick={this.props.onToggleTasks}>
                             {tasksToggleTitle}
                         </a>
                     </div>
 
-                    <div className="checkbox col-md-2">
+                    <div className='checkbox col-md-2'>
                         <label>
                             <input
-                            type="checkbox"
+                            type='checkbox'
                             checked={this.props.active}
                             onChange={this.props.onChangeActivity}>
                                 Include into total estimation
@@ -36,7 +38,7 @@ class Story extends React.Component {
                         </label>
                     </div>
 
-                    <div className="col-md-3">
+                    <div className='col-md-3'>
                         <p className={estimationTextStyle}>
                             75%-likely doable within {estimation} h
                         </p>
