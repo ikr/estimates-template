1f245fa54f38dc81438921b7ff0a65579531c6ff
Ivan Krechetov <ikr@ikr.su>
2015-12-04 14:43:57 +0100

    Lint estimateTasks

--- src/estimateTasks.js
+++ src/estimateTasks.js
@@ -1,16 +1,10 @@
 import {total75PercentLikelyHours} from 'estimate-tasks';
 
-const estimateTasks = tasks => {
-    return Math.round(total75PercentLikelyHours(
-        tasks.map(task => {
-            return {
-                bestCaseHours: task[1],
-                mostLikelyCaseHours: (task[1] + task[2]) / 2.0,
-                worstCaseHours: task[2],
-                confidencePercent: 95
-            };
-        })
-    ));
-};
-
-export default estimateTasks;
+export default tasks => Math.round(total75PercentLikelyHours(
+    tasks.map(task => ({
+        bestCaseHours: task[1],
+        mostLikelyCaseHours: (task[1] + task[2]) / 2.0,
+        worstCaseHours: task[2],
+        confidencePercent: 95
+    }))
+));
