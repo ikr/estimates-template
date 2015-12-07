8ab48171f09e320cba209b74f83d3526ee17a4b7
Ivan Krechetov <ikr@ikr.su>
2015-12-07 09:50:05 +0100

    Destructure

--- src/estimateTasks.js
+++ src/estimateTasks.js
@@ -1,10 +1,10 @@
 import {total75PercentLikelyHours} from 'estimate-tasks';
 
 export default tasks => Math.round(total75PercentLikelyHours(
-    tasks.map(task => ({
-        bestCaseHours: task[1],
-        mostLikelyCaseHours: (task[1] + task[2]) / 2.0,
-        worstCaseHours: task[2],
+    tasks.map(([, bestCaseHours, worstCaseHours]) => ({
+        bestCaseHours,
+        mostLikelyCaseHours: (bestCaseHours + worstCaseHours) / 2.0,
+        worstCaseHours,
         confidencePercent: 95
     }))
 ));
