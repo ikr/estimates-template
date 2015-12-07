9b0b443cee8aae429c06cdef01907cc47cb5557e
Ivan Krechetov <ikr@ikr.su>
2015-12-07 10:24:39 +0100

    Make StoriesTotal a stateless component

--- src/StoriesTotal.js
+++ src/StoriesTotal.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import estimateTasks from './estimateTasks';
 
-class StoriesTotal extends React.Component {
-    render() {
-        const estimation = estimateTasks(this.props.allTasks);
-
-        return (
-            <p className='lead'>
-                Grand total: 75%-likely doable within {estimation} h
-            </p>
-        );
-    }
-}
-
-export default StoriesTotal;
+export default ({allTasks}) => (
+    <p className='lead'>
+        Grand total: 75%-likely doable within {estimateTasks(allTasks)} h
+    </p>
+);
